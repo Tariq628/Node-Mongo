7010ceff3c0539d4374c0d5fbe511368975cc585
Tariq Ahmed <tariqkaleri628@gmail.com>
2023-07-23 19:16:00 +0500

    Cors enabled

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors()); // Enable all CORS requests
 app.use(express.json());
 
 // Connect to MongoDB Atlas
