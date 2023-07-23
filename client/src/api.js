192afd25fa8e72a4465437b7061432178d1bab82
Tariq Ahmed <tariqkaleri628@gmail.com>
2023-07-23 20:55:17 +0500

    Modify localhost url with production

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000", // Replace this with your backend server URL
+  baseURL: "https://panicky-tan-raven.cyclic.app", // Replace this with your backend server URL
 });
 
 export const fetchData = async () => {
